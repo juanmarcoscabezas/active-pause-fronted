21413e72955c24e79d76234e376f40c2fdad4a38
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-25 15:47:04 -0500

    created Login and Signup forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Form.css";
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function submitForm(e) {
+    e.preventDefault();
+    fetch('https://localhost:3000/auth/login')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
   return (
     <div className="Login">
-        <h1>Welcome to Login Page</h1>
+        <h2 className="page-title">ActivePause</h2>
+        <form className="login-form" onSubmit={submitForm}> 
+            <h3>Login</h3>
+            <div className="twelve columns">
+              <label htmlFor="exampleEmailInput">Your email</label>
+              <input className="u-full-width" type="email" placeholder="test@mailbox.com" name="email"/>
+            </div>
+            <div className="twelve columns">
+              <label htmlFor="exampleEmailInput">Your password</label>
+              <input className="u-full-width" type="password" placeholder="mypassword" name="password"/>
+            </div>
+            <div className="twelve columns">
+              <input className="button-primary" type="submit" value="Submit"/>
+            </div>
+        </form>
     </div>
   );
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import "./Form.css";
 
 function Signup() {
   return (
     <div className="Signup">
-        <h1>Welcome to Signup Page</h1>
+        <h2 className="page-title">ActivePause Registration</h2>
+        <form className="login-form"> 
+            <h3>Signup</h3>
+            <div class="twelve columns">
+              <label for="exampleEmailInput">First name</label>
+              <input class="u-full-width" type="email" placeholder="Juan Marcos" name="email"/>
+            </div>
+            <div class="twelve columns">
+              <label for="exampleEmailInput">last name</label>
+              <input class="u-full-width" type="email" placeholder="Cabezas" name="email"/>
+            </div>
+            <div class="twelve columns">
+              <label for="exampleEmailInput">Your email</label>
+              <input class="u-full-width" type="email" placeholder="test@mailbox.com" name="email"/>
+            </div>
+            <div class="twelve columns">
+              <label for="exampleEmailInput">Your password</label>
+              <input class="u-full-width" type="password" placeholder="mypassword" name="password"/>
+            </div>
+        </form>
     </div>
   );
 }
